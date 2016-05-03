49c983b21587c28ba36be3f2f8570d8252c3d1af
Gurwinder Singh <gurwinder.61ds@gmail.com>
2016-05-03 12:10:15 +0530

    first time

---
+++ _protected/frontend/views/layouts/footer.php
@@ -0,0 +1,60 @@
+   <!-- footer start-->
+<div class="footer">
+	<div class="container">
+        <div class="row">
+            <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+				<ul class="f-nav privacy">
+                			<li><a href="#">Privacy Policy</a></li>
+                 			<li><a href="#">FAQs</a></li>
+                  			<li><a href="cart-detail-shipping.html">Shipping & Returns</a></li>
+                            <li><a href="shoe-care.html">Shoe Care</a></li>
+                            <li><a href="size-finder.html">Sizing</a></li>
+                            <li><a href="#">Contact Us</a></li>         
+                </ul>
+                
+                <ul class="f-nav">
+                 			<li><a href="#"> Site Map</a></li>
+                            <li><a href="why-leather.html">Why Leather</a></li>
+                            <li><a href="#">Store Locator</a></li>
+                            <li><a href="csr.html"> CSR</a></li>
+                            <li><a href="#">R&D </a></li>  
+                            <li><a href="foot-health-children.html"> Foot Survege</a></li>      
+                </ul>
+                <ul class="f-nav">
+                 			
+                            <li><a href="culture.html">Culture, USPs</a></li>
+                            <li><a href="carftmanship.html"> Craftsmanship</a></li>
+                            <li><a href="our-story.html">Our Story</a></li>
+                            <li><a href="quality-control.html">Quality Control</a></li>  
+                             <li><a href="#">Terms & Condition</a></li>
+                            <li><a href="#">Tell a friend</a></li>       
+                </ul>
+                <ul class="f-nav">
+                 		<li><a href="#">CUSTOMER CARE: </a></li>
+                            <li><a href="#">jannatrosha@drish.com</a></li>   
+                            <li><a href="#">Toll Free: 1800 137 0107</a></li>   
+                            <li><a href="#">Free Shipping + Free Returns</a></li>   
+							
+                </ul>
+            
+			</div>
+                  <div class="col-lg-3 col-md-2 col-sm-12 col-xs-12 f-social">
+                   <div class="foot-socials">
+                        <ul>
+                                 <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                 <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                 <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                 <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>  
+								<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                            <li><a href="#" ><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+                              <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div><!--end foot-social-->
+					 <a href="index.html" class="f-logo-m"><img src="images/footer-logo.png" alt="Drish" class="img-responsive foot-logo"></a>
+                    
+                    <p class="copyright">© 2015 drish.  All rights reserved.</p>
+                </div><!--end col-lg-2-->
+        </div><!--end row-->
+    </div><!--end container-fluid-->
+</div>      
+ <!-- footer end-->  
