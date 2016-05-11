1fc3ec5d4190a7e7039990a92dd977f62f4b38f8
Gurwinder Singh <gurwinder.61ds@gmail.com>
2016-05-11 15:48:13 +0530

    login & register frontend

---
+++ _protected/frontend/widgets/views/product-featured.php
@@ -0,0 +1,69 @@
+<?php
+use common\models\Product;
+if($product_ids){
+?> 
+
+                        
+<ul class="grid cs-style-2">
+		<?php
+		 foreach($product_ids as $product){  
+		 ?>
+			<li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li>
+			<li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li>
+			<li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li><li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li><li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li>
+			<li>
+                <figure>
+                   <img class="img-responsive" alt="bag" src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>">
+                   <figcaption>
+                      <h3><?= $product->name ?></h3>
+                      <a href="http://dribbble.com/shots/1115960-Music">Shop Now</a>
+					</figcaption>
+                </figure>
+            </li>
+			
+		<?php	
+				}
+			
+		?>
+</ul>
+<?php } ?>
---
+++ _protected/frontend/widgets/views/product-slider.php
@@ -0,0 +1,69 @@
+<?php
+use common\models\Product;
+if($product_ids){
+?> 
+<ul class="bxslider-1 grid cs-style-2">
+		<?php
+		 foreach($product_ids as $product){  
+		 ?>
+			<li>
+                <a href="category.html">
+                   <figure>
+                      <img src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>" />
+                      <figcaption>
+                         <span><?= $product->name ?><br><i class="fa fa-inr"></i> <?= $product->price ?></span>
+                         <button type="button">Shop Now</button>
+                      </figcaption>
+                   </figure>
+                </a>
+            </li>
+			<li>
+                <a href="category.html">
+                   <figure>
+                      <img src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>" />
+                      <figcaption>
+                         <span><?= $product->name ?><br><i class="fa fa-inr"></i> <?= $product->price ?></span>
+                         <button type="button">Shop Now</button>
+                      </figcaption>
+                   </figure>
+                </a>
+            </li>
+			<li>
+                <a href="category.html">
+                   <figure>
+                      <img src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>" />
+                      <figcaption>
+                         <span><?= $product->name ?><br><i class="fa fa-inr"></i> <?= $product->price ?></span>
+                         <button type="button">Shop Now</button>
+                      </figcaption>
+                   </figure>
+                </a>
+            </li>
+			<li>
+                <a href="category.html">
+                   <figure>
+                      <img src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>" />
+                      <figcaption>
+                         <span><?= $product->name ?><br><i class="fa fa-inr"></i> <?= $product->price ?></span>
+                         <button type="button">Shop Now</button>
+                      </figcaption>
+                   </figure>
+                </a>
+            </li>
+			<li>
+                <a href="category.html">
+                   <figure>
+                      <img src="<?= Yii::$app->params['baseurl'] ?>/uploads/product/main/<?= $product->id ?>/large/<?= $product->productImages[0]->main_image ?>" />
+                      <figcaption>
+                         <span><?= $product->name ?><br><i class="fa fa-inr"></i> <?= $product->price ?></span>
+                         <button type="button">Shop Now</button>
+                      </figcaption>
+                   </figure>
+                </a>
+            </li>
+			
+		<?php	
+			}
+		?>
+</ul>
+<?php } ?>
